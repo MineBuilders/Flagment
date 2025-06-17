45dddd3215f02c5a20b8ae5c8cbf6e85be4b9c02
Cdm2883 <Cdm2883@hotmail.com>
2025-06-18 01:06:31 +0800

    build: setup basic eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,26 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import importPlugin from "eslint-plugin-import";
+import tseslint from 'typescript-eslint';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const eslintConfig = tseslint.config(
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    tseslint.configs.strict,
+    importPlugin.flatConfigs.typescript,
+    {
+        rules: {
+            semi: [ 'error', 'always' ],
+            'import/order': [ 'error', { alphabetize: { order: 'asc' } } ],
+        },
+    },
+);
 
 export default eslintConfig;
